1a8f7c6ce457c819912cd32fd9599b4d6af60ffd
nmatkows <nicolas.matkowski@nysasphere.net>
2019-02-25 21:26:37 +0100

    update font_selector gi imports

--- keepnote/gui/font_selector.py
+++ keepnote/gui/font_selector.py
@@ -27,7 +27,7 @@
 # GObject introspection imports
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+from gi.repository import GObject, Gtk
 
 
 class FontSelector (Gtk.ComboBox):
